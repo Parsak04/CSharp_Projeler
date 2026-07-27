cc1813d6142bd5084c10cc4e749bc3aba7a1bbb5
Muhammed Sena Parsak <muhammedparsak04@gmail.com>
2026-07-27 19:08:39 +0300

    Sayı sıralama kayıt.

---
+++ 1-sayi_siralama/ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Kaç adet sayı sıralamak istersiniz? : ");
+            int adet = int.Parse(Console.ReadLine());
+
+            int[] sayilar = new int[adet];
+
+            for(int i = 0; i < adet; i++)
+            {
+                Console.Write($"{i + 1}. sayıyı giriniz: ");
+                sayilar[i] = int.Parse(Console.ReadLine());
+            }
+
+            Array.Sort(sayilar);
+            Console.WriteLine();
+
+            Console.Write("Sayıların sıralanmış hali: ");
+            
+            foreach (int s in sayilar)
+            {
+                Console.Write(s + " ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}

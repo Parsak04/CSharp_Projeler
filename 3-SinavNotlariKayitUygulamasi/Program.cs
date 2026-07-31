25fb81855297b2e98183ec861ddb9ddaa57d75d4
Muhammed Sena Parsak <muhammedparsak04@gmail.com>
2026-07-31 18:37:39 +0300

    Yeni proje eklendi.

---
+++ 3-SinavNotlariKayitUygulamasi/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_SinavNotlariKayitUygulamasi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--------- Sınav Notları Kayıt Uygulaması --------");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+
+            //Sınıv mevcudu ve öğrenci isimlerini alıyoruz
+            Console.WriteLine("-------------------------------------------------");
+            Console.WriteLine();
+            Console.Write("Sınıfınızda kaç öğrenci var?: ");
+            int studentCount = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine("-------------------------------------------------");
+
+            string[] studentNames = new string[studentCount];
+            double[] totalExamAvg = new double[studentCount];
+
+            for(int i = 0; i < studentCount; i++)
+            {
+                Console.Write($"{i + 1}. öğrencinin adı: ");
+                studentNames[i] = Console.ReadLine();
+
+                double totalExamResult = 0;
+
+                //Sınav notlarını alıyoruz
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write($"{studentNames[i]} adlı öğrenci {j + 1}. sınav notu: ");
+                    double value = double.Parse(Console.ReadLine());
+                    totalExamResult += value;
+                }
+                Console.WriteLine();
+                totalExamAvg[i] = totalExamResult / 3;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("----------- Öğrencilerin Ortalamaları -----------");
+
+            //Öğrencilerin ortalaması ve geçip kalma durumunu yazdırıyoruz
+            for (int i = 0;i < studentCount;i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"{studentNames[i]} adlı öğrencinin ortalaması: {totalExamAvg[i]:F2}");
+                if (totalExamAvg[i] >= 50)
+                {
+                    Console.WriteLine($"{studentNames[i]} adlı öğrenci geçti.");
+                }
+                else
+                {
+                    Console.WriteLine($"{studentNames[i]} adlı öğrenci kaldı.");
+                }
+            }
+
+
+
+
+            Console.Read();
+        }
+    }
+}
---
+++ 3-SinavNotlariKayitUygulamasi/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("3-SinavNotlariKayitUygulamasi")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("3-SinavNotlariKayitUygulamasi")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("4a4ec047-de1b-45f3-a4e1-6eeb1e5c970a")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
